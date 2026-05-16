ed00571d41809af43b2f81c6431a25fb41df621f
Quinn Turner <quinnturnertech@gmail.com>
2026-05-16 13:23:59 -0300

    Update deps

--- test/yarn-auditor.ts
+++ test/yarn-auditor.ts
@@ -70,235 +70,231 @@ export function performAuditTests({ yarnAbsolutePath, yarnVersion }: PerformAudi
 
   // To modify what slow times are, need to use
   // function() {} instead of () => {}
-  describe(
-    `yarn-${majorVersion}-auditor`,
-    function testYarnAuditor() {
-      it("prints full report with critical severity", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-critical`),
-            levels: { critical: true },
-            "report-type": "full",
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            failedLevelsFound: ["critical"],
-            advisoriesFound: ["GHSA-28xh-wpgr-7fm8"],
-            advisoryPathsFound: ["GHSA-28xh-wpgr-7fm8|open"],
-          }),
-        );
-      });
-      it("does not report critical severity if it set to false", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-critical`),
-            levels: { critical: false },
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(summaryWithDefault());
-      });
-      it("reports summary with high severity", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-high`),
-            levels: { high: true },
-            "report-type": "summary",
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            failedLevelsFound: ["high"],
-            advisoriesFound: ["GHSA-hrpp-h998-j3pp"],
-            advisoryPathsFound: ["GHSA-hrpp-h998-j3pp|qs"],
-          }),
-        );
-      });
-      it("reports important info with moderate severity", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-moderate`),
-            allowlist: new Allowlist([]),
-            levels: { moderate: true },
-            "report-type": "important",
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            failedLevelsFound: ["moderate"],
-            allowlistedAdvisoriesFound: [],
-            advisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
-            advisoryPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
-          }),
-        );
-      });
-      it("does not report moderate severity if it set to false", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-moderate`),
-            levels: { moderate: false },
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(summaryWithDefault());
-      });
-      it("ignores an advisory if it is allowlisted", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-moderate`),
-            levels: { moderate: true },
-            allowlist: new Allowlist(["GHSA-rvg8-pwq2-xj7q"]),
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            allowlistedAdvisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
-          }),
-        );
-      });
-      it("ignores an advisory if it is allowlisted using a NSPRecord", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-moderate`),
-            levels: { moderate: true },
-            allowlist: new Allowlist([{ "GHSA-rvg8-pwq2-xj7q": { active: true } }]),
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            allowlistedAdvisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
-          }),
-        );
-      });
-      it("does not ignore an advisory that is not allowlisted", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-moderate`),
-            levels: { moderate: true },
-            allowlist: new Allowlist(["GHSA-cff4-rrq6-h78w"]),
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            allowlistedAdvisoriesNotFound: ["GHSA-cff4-rrq6-h78w"],
-            failedLevelsFound: ["moderate"],
-            advisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
-            advisoryPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
-          }),
-        );
-      });
-      it("does not ignore an advisory that is not allowlisted using a NSPRecord", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-moderate`),
-            levels: { moderate: true },
-            allowlist: new Allowlist([
-              "GHSA-cff4-rrq6-h78w",
-              { "GHSA-rvg8-pwq2-xj7q": { active: false } },
-            ]),
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            allowlistedAdvisoriesNotFound: ["GHSA-cff4-rrq6-h78w"],
-            failedLevelsFound: ["moderate"],
-            advisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
-            advisoryPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
-          }),
-        );
-      });
-      it("ignores an advisory that has not expired", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-moderate`),
-            levels: { moderate: true },
-            allowlist: new Allowlist([
-              {
-                "GHSA-rvg8-pwq2-xj7q|base64url": {
-                  active: true,
-                  expiry: new Date(Date.now() + 9000).toISOString(),
-                },
+  describe(`yarn-${majorVersion}-auditor`, { timeout: 10_000 }, function testYarnAuditor() {
+    it("prints full report with critical severity", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-critical`),
+          levels: { critical: true },
+          "report-type": "full",
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          failedLevelsFound: ["critical"],
+          advisoriesFound: ["GHSA-28xh-wpgr-7fm8"],
+          advisoryPathsFound: ["GHSA-28xh-wpgr-7fm8|open"],
+        }),
+      );
+    });
+    it("does not report critical severity if it set to false", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-critical`),
+          levels: { critical: false },
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(summaryWithDefault());
+    });
+    it("reports summary with high severity", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-high`),
+          levels: { high: true },
+          "report-type": "summary",
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          failedLevelsFound: ["high"],
+          advisoriesFound: ["GHSA-hrpp-h998-j3pp"],
+          advisoryPathsFound: ["GHSA-hrpp-h998-j3pp|qs"],
+        }),
+      );
+    });
+    it("reports important info with moderate severity", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-moderate`),
+          allowlist: new Allowlist([]),
+          levels: { moderate: true },
+          "report-type": "important",
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          failedLevelsFound: ["moderate"],
+          allowlistedAdvisoriesFound: [],
+          advisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
+          advisoryPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
+        }),
+      );
+    });
+    it("does not report moderate severity if it set to false", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-moderate`),
+          levels: { moderate: false },
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(summaryWithDefault());
+    });
+    it("ignores an advisory if it is allowlisted", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-moderate`),
+          levels: { moderate: true },
+          allowlist: new Allowlist(["GHSA-rvg8-pwq2-xj7q"]),
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          allowlistedAdvisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
+        }),
+      );
+    });
+    it("ignores an advisory if it is allowlisted using a NSPRecord", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-moderate`),
+          levels: { moderate: true },
+          allowlist: new Allowlist([{ "GHSA-rvg8-pwq2-xj7q": { active: true } }]),
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          allowlistedAdvisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
+        }),
+      );
+    });
+    it("does not ignore an advisory that is not allowlisted", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-moderate`),
+          levels: { moderate: true },
+          allowlist: new Allowlist(["GHSA-cff4-rrq6-h78w"]),
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          allowlistedAdvisoriesNotFound: ["GHSA-cff4-rrq6-h78w"],
+          failedLevelsFound: ["moderate"],
+          advisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
+          advisoryPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
+        }),
+      );
+    });
+    it("does not ignore an advisory that is not allowlisted using a NSPRecord", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-moderate`),
+          levels: { moderate: true },
+          allowlist: new Allowlist([
+            "GHSA-cff4-rrq6-h78w",
+            { "GHSA-rvg8-pwq2-xj7q": { active: false } },
+          ]),
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          allowlistedAdvisoriesNotFound: ["GHSA-cff4-rrq6-h78w"],
+          failedLevelsFound: ["moderate"],
+          advisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
+          advisoryPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
+        }),
+      );
+    });
+    it("ignores an advisory that has not expired", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-moderate`),
+          levels: { moderate: true },
+          allowlist: new Allowlist([
+            {
+              "GHSA-rvg8-pwq2-xj7q|base64url": {
+                active: true,
+                expiry: new Date(Date.now() + 9000).toISOString(),
               },
-            ]),
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            allowlistedPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
-          }),
-        );
-      });
-      it("does not ignore an advisory that has expired", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-moderate`),
-            levels: { moderate: true },
-            allowlist: new Allowlist([
-              "GHSA-cff4-rrq6-h78w",
-              {
-                "GHSA-rvg8-pwq2-xj7q": {
-                  active: true,
-                  expiry: new Date(Date.now() - 9000).toISOString(),
-                },
+            },
+          ]),
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          allowlistedPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
+        }),
+      );
+    });
+    it("does not ignore an advisory that has expired", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-moderate`),
+          levels: { moderate: true },
+          allowlist: new Allowlist([
+            "GHSA-cff4-rrq6-h78w",
+            {
+              "GHSA-rvg8-pwq2-xj7q": {
+                active: true,
+                expiry: new Date(Date.now() - 9000).toISOString(),
               },
-            ]),
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            allowlistedAdvisoriesNotFound: ["GHSA-cff4-rrq6-h78w"],
-            failedLevelsFound: ["moderate"],
-            advisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
-            advisoryPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
-          }),
-        );
-      });
-      it("reports low severity", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-low`),
-            levels: { low: true },
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(
-          summaryWithDefault({
-            failedLevelsFound: ["low"],
-            advisoriesFound: ["GHSA-c6rq-rjc2-86v2"],
-            advisoryPathsFound: ["GHSA-c6rq-rjc2-86v2|chownr"],
-          }),
-        );
-      });
-      it("passes with no vulnerabilities", async () => {
-        const summary = await audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-none`),
-            levels: { low: true },
-          }),
-          (_summary) => _summary,
-        );
-        expect(summary).to.eql(summaryWithDefault());
-      });
-      it("doesn't use the registry flag since it's not supported in Yarn yet", () =>
-        audit(
-          config({
-            directory: testDirectory(`yarn-${majorVersion}-low`),
-            levels: { low: true },
-            registry: "https://example.com",
-          }),
-          (_summary) => _summary,
-        ));
-    },
-    { timeout: 10_000 },
-  );
+            },
+          ]),
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          allowlistedAdvisoriesNotFound: ["GHSA-cff4-rrq6-h78w"],
+          failedLevelsFound: ["moderate"],
+          advisoriesFound: ["GHSA-rvg8-pwq2-xj7q"],
+          advisoryPathsFound: ["GHSA-rvg8-pwq2-xj7q|base64url"],
+        }),
+      );
+    });
+    it("reports low severity", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-low`),
+          levels: { low: true },
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(
+        summaryWithDefault({
+          failedLevelsFound: ["low"],
+          advisoriesFound: ["GHSA-c6rq-rjc2-86v2"],
+          advisoryPathsFound: ["GHSA-c6rq-rjc2-86v2|chownr"],
+        }),
+      );
+    });
+    it("passes with no vulnerabilities", async () => {
+      const summary = await audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-none`),
+          levels: { low: true },
+        }),
+        (_summary) => _summary,
+      );
+      expect(summary).to.eql(summaryWithDefault());
+    });
+    it("doesn't use the registry flag since it's not supported in Yarn yet", () =>
+      audit(
+        config({
+          directory: testDirectory(`yarn-${majorVersion}-low`),
+          levels: { low: true },
+          registry: "https://example.com",
+        }),
+        (_summary) => _summary,
+      ));
+  });
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   test: {
     coverage: {
       provider: "v8",
+      thresholds: {
+        statements: 72,
+        branches: 84,
+        functions: 76,
+        lines: 71,
+      },
     },
   },
 });
